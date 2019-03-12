24a075690cc3eca10c1c1820b408c88e27bc6c7f
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-12 13:51:45 +0000

    MVP: Complete Login functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,32 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 
-const Login = () => {
+const handleClick = loginRequest => (event) => {
+  event.preventDefault();
+  let username = '';
+  let password = '';
+  event.target.parentNode.childNodes.forEach((childNode) => {
+    if (childNode.name === 'username') {
+      username = childNode.value;
+    } else if (childNode.name === 'password') {
+      password = childNode.value;
+    }
+  });
+  if (username && password) {
+    loginRequest(username, password);
+  }
+};
+
+const Login = ({ loggedIn, loginRequest }) => {
+  if (loggedIn) {
+    return <Redirect to="/dashboard" />;
+  }
   return (
     <div className="login-wrapper">
       <div className="login">
-        
         <form className="login-form">
           <h2>Art Portolio</h2>
           <p>Username</p>
@@ -21,15 +43,20 @@ const Login = () => {
 
           </div>
 
-          <button type="submit" className="login-button" onClick>LOGIN</button>
+          <button type="submit" className="login-button" onClick={handleClick(loginRequest)}>LOGIN</button>
 
           <div className="login-page-register">
-            <p>Need an account?<a>Sign up now!</a></p>
+            <p>Need an account?<a href="#">Sign up now!</a></p>
           </div>
         </form>
       </div>
     </div>
   );
+};
+
+Login.propTypes = {
+  loginRequest: PropTypes.func.isRequired,
+  loggedIn: PropTypes.bool.isRequired,
 };
 
 export default Login;
