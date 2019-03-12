3b48719b36a9ce24726a248af2a3fcd226437e19
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-12 07:47:12 +0000

    MVP: Test CRUD operations

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { createLogger } from 'redux-logger';
-import thunk from 'redux-thunk';
 import { BrowserRouter as Router } from 'react-router-dom';
+import thunk from 'redux-thunk';
+import rootReducer from './store/reducers';
+
 import './index.css';
+import * as actions from './store/actions';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -13,11 +16,25 @@ const logger = createLogger({
   collapsed: true,
 });
 
-const store = createStore(() => {}, applyMiddleware(
+const store = createStore(rootReducer, applyMiddleware(
   thunk, logger,
 ));
 
-store.dispatch({ type: 'HELLO' });
+const token = localStorage.getItem('token');
+if (token) {
+  store.dispatch(actions.loginSuccess(token));
+}
+
+// store.dispatch({ type: 'HELLO' });
+// store.dispatch(actions.fetchAllPosts());
+// store.dispatch(actions.createPost('hds', 'hdsh', 'hda'));
+// store.dispatch(actions.updatePost('dsdsa', 'dsdsa', 'dsads'));
+// store.dispatch(actions.deletePost(1));
+// store.dispatch(actions.loginRequest('brooks', '1234'));
+store.dispatch(actions.fetchAllPosts());
+// window.setTimeout(() => store.dispatch(actions.updatePost(6, 'asdf1', 'asdf1', 'asdf1')), 3000);
+// window.setTimeout(() => store.dispatch(actions.createPost('asdf2', 'asdf2', 'asdf2')), 3000);
+// window.setTimeout(() => store.dispatch(actions.deletePost(2)), 3000);
 
 ReactDOM.render(
   <Provider store={store}>
