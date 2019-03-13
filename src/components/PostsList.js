83b5745f7c3951abe2f43dc8f33a90950717d549
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-13 08:21:53 +0000

    MVP: Pass down props to PostModal

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -2,13 +2,27 @@ import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 import Post from './Post';
+import PostModal from './PostModal';
 
 const PostsList = (props) => {
-  const { posts } = props;
+  const {
+    posts, deletePost, updatePost, showPostModal, shownPostModal, hidePostModal, isEditable,
+    makePostModalEditable, makePostModalUneditable,
+  } = props;
   return (
     <div className="post-list-wrapper">
       <h3>This is a where the gallery goes</h3>
       <div className="post-list">
+        <PostModal
+          isEditable={isEditable}
+          posts={posts}
+          shownPostModal={shownPostModal}
+          hidePostModal={hidePostModal}
+          deletePost={deletePost}
+          updatePost={updatePost}
+          makePostModalEditable={makePostModalEditable}
+          makePostModalUneditable={makePostModalUneditable}
+        />
         {posts.map(post => (
           <Post
             id={post.id}
@@ -18,8 +32,10 @@ const PostsList = (props) => {
             userId={post.userId}
             description={post.description}
             postName={post.postName}
-            deletePost={post.deletePost}
-            updatePost={post.updatePost}
+            deletePost={deletePost}
+            updatePost={updatePost}
+            showPostModal={showPostModal}
+            shownPostModal={shownPostModal}
           />
         ))}
       </div>
@@ -29,6 +45,14 @@ const PostsList = (props) => {
 
 PostsList.propTypes = {
   posts: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  showPostModal: PropTypes.func.isRequired,
+  hidePostModal: PropTypes.func.isRequired,
+  deletePost: PropTypes.func.isRequired,
+  updatePost: PropTypes.func.isRequired,
+  shownPostModal: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]).isRequired,
+  isEditable: PropTypes.bool.isRequired,
+  makePostModalEditable: PropTypes.func.isRequired,
+  makePostModalUneditable: PropTypes.func.isRequired,
 };
 
 export default PostsList;
