88df3282e8391ca294e9afb4a02ae2a5b316b3a6
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-13 08:17:13 +0000

    MVP: Create Post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,51 +1,40 @@
 import React from 'react';
-import ImagePost from '../assets/artist.png';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
+// import ImagePost from '../assets/artist.png';
+
+const handleShowModalClick = (id, showPostModal) => (event) => {
+  event.preventDefault();
+  showPostModal(id);
+};
 
 const Post = (props) => {
-  const { id, imageUrl, upvotes, userId, description, postName, deletePost, updatePost } = props;
+  const {
+    id, imageUrl, upvotes, description, showPostModal,
+  } = props;
 
   return (
-    <div className="post-container">
-
-      <div className="post-overlay">
-        <div className="overlay-inner">
-          <button className="close" type="button">✕</button>
-          <img
-            key={id}
-            id={id}
-            src={imageUrl}
-            upvotes={upvotes}
-            description={description}
-            alt="cannot display post"
-          />
-          <div className="details">
-            <div className="upvote"><i className="fas fa-heart"></i>{upvotes} Likes</div>
-            <span>By Current User</span>
-          </div>
-          <p>Selvage raclette hoodie succulents kogi fingerstache readymade viral you probably haven't heard of them. </p>         
-        </div>
-      </div>
-
-      <div className="post">
-        <img
-          key={id}
-          id={id}
-          src={imageUrl}
-          upvotes={upvotes}
-          description={description}
-          alt="cannot display post"
-        />
-        <p>{upvotes} Likes</p>
-        <div className="post-action-buttons">
-          <button type="button" className="delete">DELETE</button>
-          <button type="button" className="edit">EDIT</button>
-        </div>
-      </div>
-      
-      
+    <div className="post">
+      <button type="button" onClick={handleShowModalClick(id, showPostModal)}>View</button>
+      <img src={imageUrl} alt={description} />
+      <p>
+        {upvotes}
+        {' '}
+Likes
+      </p>
     </div>
 
   );
+};
+
+Post.propTypes = {
+  id: PropTypes.number.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  upvotes: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  deletePost: PropTypes.func.isRequired,
+  updatePost: PropTypes.func.isRequired,
+  showPostModal: PropTypes.func.isRequired,
 };
 
 export default Post;
