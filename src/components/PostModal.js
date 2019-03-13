63b863d9d64c47f9854a490f3def61c04f6ae81a
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-13 16:07:28 +0000

    MVP: Refactor PostModal and fix logic issues

--- src/components/PostModal.js
+++ src/components/PostModal.js
@@ -1,99 +1,113 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
-
-const handleClick = hidePostModal => () => hidePostModal();
-
-const handleEditClick = makePostModalEditable => (event) => {
-  event.preventDefault();
-  makePostModalEditable();
-};
-
-const handleDeleteClick = (id, deletePost, hidePostModal) => (event) => {
-  event.preventDefault();
-  deletePost(id);
-  hidePostModal();
-};
-
-
-// onst updatePost = (id, postName, imageUrl, description)
-
-
-const handleSave = (id, updatePost) => (event) => {
-  event.preventDefault();
-  // updatePost(id, 'asdf', 'dshad', 'dada');
-  let postName = '';
-  let imageUrl = '';
-  let description = '';
-  event.target.parentNode.parentNode.childNodes.forEach((childNode) => {
-    if (childNode.name === 'postName') {
-      postName = childNode.value;
-    } else if (childNode.name === 'imageUrl') {
-      imageUrl = childNode.value;
-    } else if (childNode.name === 'description') {
-      description = childNode.value;
-    }
-  });
-  if (postName || description || imageUrl) {
-    updatePost(id, postName, imageUrl, description);
-  }
-};
+import PostModalEditable from './PostModalEditable';
+import PostModalUneditable from './PostModalUneditable';
 
 const PostModal = (props) => {
   const {
     shownPostModal, posts, hidePostModal, deletePost, isEditable, makePostModalEditable,
-    makePostModalUneditable, updatePost,
+    makePostModalUneditable, updatePost, loggedIn,
   } = props;
-  if (shownPostModal && !isEditable) {
-    const post = posts.find(aPost => aPost.id === shownPostModal);
-    console.log(post);
+
+  if (loggedIn && shownPostModal) {
     return (
-      <div className="post-modal-container">
-I am a post modal.
-        <button type="button" onClick={handleClick(hidePostModal)}>X</button>
-        <img src={post.imageUrl} alt="cannot display post" />
-        <p>
-          {post.upvotes}
-          {' '}
-Likes
-        </p>
-        <div className="post-action-buttons">
-          <button type="button" className="delete" onClick={handleDeleteClick(shownPostModal, deletePost, hidePostModal)}>DELETE</button>
-          <button type="button" className="edit" onClick={handleEditClick(makePostModalEditable)}>EDIT</button>
-        </div>
-        <p>{post.postName}</p>
-        <p>{post.description}</p>
-      </div>
+      <PostModalEditable
+        isEditable={isEditable}
+        posts={posts}
+        shownPostModal={shownPostModal}
+        hidePostModal={hidePostModal}
+        deletePost={deletePost}
+        updatePost={updatePost}
+        makePostModalEditable={makePostModalEditable}
+        makePostModalUneditable={makePostModalUneditable}
+        loggedIn={loggedIn}
+      />
     );
   }
-  if (shownPostModal && isEditable) {
-    const post = posts.find(aPost => aPost.id === shownPostModal);
+  if (shownPostModal) {
     return (
-      <div className="post-modal-container">
-        I am a post modal.
-        <button type="button" onClick={handleClick(hidePostModal)}>X</button>
-        <form>
-          <input name="imageUrl" placeholder={post.imageUrl} />
-          <input name="postName" placeholder={post.postName} />
-          <input name="description" placeholder={post.description} />
-          <div className="post-action-buttons">
-            <button type="button" className="delete" onClick={handleDeleteClick(shownPostModal, deletePost, hidePostModal)}>DELETE</button>
-            <button type="button" className="edit" onClick={handleSave(shownPostModal, updatePost)}>SAVE</button>
-          </div>
-        </form>
-        <p>
-          {post.upvotes}
-          {' '}
-Likes
-        </p>
-
-        <p>{post.postName}</p>
-        <p>{post.description}</p>
-      </div>
+      <PostModalUneditable
+        posts={posts}
+        shownPostModal={shownPostModal}
+        hidePostModal={hidePostModal}
+      />
     );
   }
   return null;
 };
+
+//   if (shownPostModal && !isEditable) {
+//     const post = posts.find(aPost => aPost.id === shownPostModal);
+//     console.log(post);
+//     return (
+//       <div className="post-modal-container">
+// I am a post modal.
+//         <button type="button" onClick={handleClick(hidePostModal)}>X</button>
+//         <img src={post.imageUrl} alt="cannot display post" />
+//         <p>
+//           {post.upvotes}
+//           {' '}
+// Likes
+//         </p>
+//         <div className="post-action-buttons">
+//           <button type="button" className="delete" onClick={handleDeleteClick(shownPostModal, deletePost, hidePostModal)}>DELETE</button>
+//           <button type="button" className="edit" onClick={handleEditClick(makePostModalEditable)}>EDIT</button>
+//         </div>
+//         <p>{post.postName}</p>
+//         <p>{post.description}</p>
+//       </div>
+//     );
+//   }
+//   if (shownPostModal && isEditable) {
+//     const post = posts.find(aPost => aPost.id === shownPostModal);
+//     return (
+//       <div className="post-modal-container">
+//         I am a post modal.
+//         <button type="button" onClick={handleClick(hidePostModal)}>X</button>
+//         <form>
+//           <input name="imageUrl" placeholder={post.imageUrl} />
+//           <input name="postName" placeholder={post.postName} />
+//           <input name="description" placeholder={post.description} />
+//           <div className="post-action-buttons">
+//             <button type="button" className="delete" onClick={handleDeleteClick(shownPostModal, deletePost, hidePostModal)}>DELETE</button>
+//             <button type="button" className="edit" onClick={handleSave(shownPostModal, updatePost)}>SAVE</button>
+//           </div>
+//         </form>
+//         <p>
+//           {post.upvotes}
+//           {' '}
+// Likes
+//         </p>
+//
+//         <p>{post.postName}</p>
+//         <p>{post.description}</p>
+//       </div>
+//     );
+//   }
+//   if (shownPostModal & !loggedIn) {
+//     const post = posts.find(aPost => aPost.id === shownPostModal);
+//     return (
+//       <div className="post-modal-container">
+//         I am a post modal.
+//         <button type="button" onClick={handleClick(hidePostModal)}>X</button>
+//         <img src={post.imageUrl} alt="cannot display post" />
+//         <p>
+//           {post.upvotes}
+//           {' '}
+//           Likes
+//         </p>
+//         {/*<div className="post-action-buttons">*/}
+//           {/*<button type="button" className="delete" onClick={handleDeleteClick(shownPostModal, deletePost, hidePostModal)}>DELETE</button>*/}
+//           {/*<button type="button" className="edit" onClick={handleEditClick(makePostModalEditable)}>EDIT</button>*/}
+//         {/*</div>*/}
+//         <p>{post.postName}</p>
+//         <p>{post.description}</p>
+//       </div>
+//     )
+//   }
+//   return null;
+// };
 
 PostModal.propTypes = {
   shownPostModal: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]).isRequired,
@@ -106,6 +120,7 @@ PostModal.propTypes = {
   makePostModalEditable: PropTypes.func.isRequired,
   makePostModalUneditable: PropTypes.func.isRequired,
   updatePost: PropTypes.func.isRequired,
+  loggedIn: PropTypes.bool.isRequired,
 };
 
 export default PostModal;
