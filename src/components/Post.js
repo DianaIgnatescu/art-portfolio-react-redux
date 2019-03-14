8b11ab7201b5df0906e491f3a5636e9652720792
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-14 10:10:16 +0000

    MVP: Add styled components for PostContainer

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,61 @@
 import React from 'react';
+import styled from 'styled-components';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 // import ImagePost from '../assets/artist.png';
+
+const PostContainer = styled.div`
+  width: 30%;
+  // margin: 15px;
+  margin-bottom: 10px;
+  @media (max-width: 500px) {
+    width: 100%;
+  }
+  .image-container {
+    @media (max-width: 500px) {
+    max-width: 100%;
+    }
+  }
+    
+  }
+  button {
+    width: 60px;
+    height: 30px;
+    font-size: 1.6rem;
+    text-align: center;
+    position: relative;
+    border-radius: 0;
+    left: 71.5%;
+    top: 30px;
+    padding: 0;
+    cursor: pointer;
+    background: #202330;
+    opacity: 0.5;
+    color: #FFFFFF;
+    &:hover {
+    opacity: 0.6;
+    color: #FFFFFF;
+    }
+    @media (max-width: 500px) {
+    left: 84.3%;
+    }
+  }
+ .post-reactions {
+    display: flex;
+    padding: 5px;
+    background: #202330;
+    opacity: 0.5;
+    position: relative;
+    bottom: 29px;
+    &:hover {
+      opacity: 0.5;
+      color: #FFFFFF;
+    }
+    i {
+      padding-right: 7px;
+    }
+  } 
+`;
 
 const handleShowModalClick = (id, showPostModal) => (event) => {
   event.preventDefault();
@@ -14,12 +68,8 @@ const Post = (props) => {
   } = props;
 
   return (
-    <div className="post">
-      <button type="button" onClick={handleShowModalClick(id, showPostModal)}>
-        <i
-          className="fas fa-expand-arrows-alt"
-        />
-      </button>
+    <PostContainer>
+      <button type="button" onClick={handleShowModalClick(id, showPostModal)}>View</button>
       <div className="image-container">
         <img src={imageUrl} alt={description} />
       </div>
@@ -37,7 +87,7 @@ Views
         </p>
       </div>
 
-    </div>
+    </PostContainer>
 
   );
 };
