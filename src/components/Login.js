e4d743043c0e4d43cec46d91cc61f9d351a7f842
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-14 10:10:36 +0000

    MVP: Add styled component for Login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,106 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
+import styled from 'styled-components';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
 import { Redirect, Link } from 'react-router-dom';
+
+const LoginWrapper = styled.div`
+  .login {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 50px 0;
+    h2 {
+      text-align: center;
+      font-family: 'Julius Sans One', sans-serif;
+    }
+    .login-form {
+      padding: 20px;
+      background: linear-gradient(to bottom,#3D3A4F, #202330);
+      border-radius: 3px;
+      border: 1px solid #676D85;
+      margin: 60px 0 20px 0;
+      @media (max-width: 500px) {
+        max-width: 100%;
+        border: none;
+        background: none;
+      }
+      p {
+      padding: 20px 0 10px;
+      }
+      button {
+        width: 100%;
+        margin: 20px 0;
+        background: #E19870;
+        border: none;
+        height: 40px;
+        font-size: 1.6rem;
+        border-radius: 3px;
+        &:hover {
+          opacity: 0.8;
+          
+          cursor: pointer;
+        }
+      }
+      span {
+        font-size: 1.6rem;
+        padding-left: 15px;
+        text-decoration: underline;
+        &:hover {
+          text-decoration: underline;
+          color: #E19870;
+          cursor: pointer;
+        }
+      }
+      input {
+        width: 100%;
+        height: 40px;
+        border-radius: 3px;
+        border: none;
+        color: #BEBEBE;
+        padding-left: 15px;
+        background:#3e3b4f;
+        font-size: 1.8rem;
+        &::placeholder {
+        font-size: 1.8rem;
+        }
+      }
+      .center-text {
+        text-align: center;
+        padding-top: 20px;
+      }
+      .alternative-login-buttons {
+        display: flex;
+        justify-content: space-between;
+        @media (max-width: 500px) {
+          flex-direction: column;
+        }
+        button {
+        max-width: 47%;
+        background: #676D85;
+        color: #BEBEBE;
+        &:hover {
+        cursor: pointer;
+        }
+        @media (max-width: 500px) {
+          max-width: 100%;
+          margin: 10px 0 0 0 ;
+        }
+      }
+    }
+    a {
+      font-size: 1.8rem;
+      text-decoration: underline;
+      padding-left: 15px;
+      &:hover {
+        text-decoration: underline;
+        color: #E19870;
+        cursor: pointer;
+      }
+    }
+  }
+`;
+
 
 const handleClick = loginRequest => (event) => {
   event.preventDefault();
@@ -25,7 +123,7 @@ const Login = ({ loggedIn, loginRequest }) => {
     return <Redirect to="/home" />;
   }
   return (
-    <div className="login-wrapper">
+    <LoginWrapper>
       <div className="login">
         <form className="login-form">
           <h2>Art Portolio</h2>
@@ -50,7 +148,7 @@ const Login = ({ loggedIn, loginRequest }) => {
           </div>
         </form>
       </div>
-    </div>
+    </LoginWrapper>
   );
 };
 
